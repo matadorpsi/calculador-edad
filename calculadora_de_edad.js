26b2f1e3dd354d288276ef4b7d767f7da9b57025
matadorpsi <124622675+matadorpsi@users.noreply.github.com>
2023-04-16 23:13:28 -0500

    solicitamos los datos actuales

--- calculadora_de_edad.js
+++ calculadora_de_edad.js
@@ -45,3 +45,37 @@ var edad = calcularEdad(fechaNacimientoObj);
 
 // Mostramos la edad en la pantalla
 alert("Tu edad es " + edad + " años.");
+
+
+// Definimos una función para calcular la edad
+function calcularEdad(fechaNacimiento) {
+      // Obtenemos la fecha actual
+      var fechaActual = new Date();
+      // Obtenemos el año actual
+      var añoActual = fechaActual.getFullYear();
+      // Obtenemos el mes actual
+      var mesActual = fechaActual.getMonth() + 1;
+      // Obtenemos el día actual
+      var diaActual = fechaActual.getDate();
+    
+      // Obtenemos el año de nacimiento
+      var añoNacimiento = fechaNacimiento.getFullYear();
+      // Obtenemos el mes de nacimiento
+      var mesNacimiento = fechaNacimiento.getMonth() + 1;
+      // Obtenemos el día de nacimiento
+      var diaNacimiento = fechaNacimiento.getDate();
+    
+      // Calculamos la edad restando el año actual menos el año de nacimiento
+      var edad = añoActual - añoNacimiento;
+    
+      // Si el mes actual es menor al mes de nacimiento, o si son iguales pero el día actual es menor que el día de nacimiento,
+      // significa que la persona todavía no ha cumplido años, por lo que restamos uno a la edad
+      if (mesActual < mesNacimiento || (mesActual == mesNacimiento && diaActual < diaNacimiento)) {
+        edad--;
+      }
+    
+      // Devolvemos la edad calculada
+      return edad;
+}
+
+
