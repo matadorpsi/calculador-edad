a7fe3a74b7f4a7e41a70d6d4c47e036efeb03262
matadorpsi <124622675+matadorpsi@users.noreply.github.com>
2023-04-16 23:06:38 -0500

    obtenemos datos de las fechas que se solicitan

--- calculadora_de_edad.js
+++ calculadora_de_edad.js
@@ -1,1 +1,16 @@
-function calcularEdad(fechaNacimiento) {}
+function calcularEdad(fechaNacimiento) {
+      // Obtenemos la fecha actual
+  var fechaActual = new Date();
+  // Obtenemos el año actual
+  var añoActual = fechaActual.getFullYear();
+  // Obtenemos el mes actual
+  var mesActual = fechaActual.getMonth() + 1;
+  // Obtenemos el día actual
+  var diaActual = fechaActual.getDate();
+// Obtenemos el año de nacimiento
+  var añoNacimiento = fechaNacimiento.getFullYear();
+  // Obtenemos el mes de nacimiento
+  var mesNacimiento = fechaNacimiento.getMonth() + 1;
+  // Obtenemos el día de nacimiento
+  var diaNacimiento = fechaNacimiento.getDate();
+}
